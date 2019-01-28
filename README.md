858001bcab1e3384d1e3d4789229d572b6796177
Gregor <HHHartmann@users.noreply.github.com>
2019-01-28 22:23:43 +0100

    add CreateSyncFunction

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # nodemcu-syncro
 add synchronous execution of commands which originally have a callback to indicate execution.
 
-Be able to white code like
+Be able to write code like
 
 ```lua
 for i = 1 to 10
@@ -12,7 +12,7 @@ end
 
 Also have preemptive multitasking:
 Start several coroutines which run synchronous code.
-
+Make sure to call wait or some other synchronous function which uses callbacks to allow other coroutines and the system to run.
 
 ```lua
 function blinkLed(pin, interval)
@@ -22,8 +22,9 @@ function blinkLed(pin, interval)
   fw.wait(interval)
 end
 
+fw = dofile("SynchronousFramework.lua")
 fw.start(blinkLed, 2, 20)    -- will call blinkLed(2, 20) in a separate coroutine
 fw.start(blinkLed, 3, 17)
 ```
 
-which will blink the leds in given intervals.
+which will blink the leds in given intervals. See Examples for another example.
--- core/SynchronousFramework.lua
+++ core/SynchronousFramework.lua
@@ -13,21 +13,33 @@ end
 
 -- Wrap the CB inside a command like wait or when getting async
 -- calls a function with a callback and handles coroutine context switch 
-framework.awaitCB = function(method)
+framework.awaitCB = function(method, ...)
   local co = coroutine.running()
-  method(function (...) resumeFromCB(co, unpack(arg)) end )
-  coroutine.yield()
+  method(function (...) resumeFromCB(co, unpack(arg)) end, ... )
+  return coroutine.yield()
 end
 
-framework.wait = function(time)
-  local timer = tmr.create()
-  framework.awaitCB( function (CB)
-                      print("timer:alarm")
-                      timer:alarm(time, tmr.ALARM_SINGLE, CB)
-                    end )
+-- creates a method which can be called with params and returns the params of the callback.
+-- func is a function which is called with a callback function as first param followed by the params given to the function
+--      it has to call the desired function
+-- the return values are the values given to the callback function
+framework.CreateSyncFunction = function(func)
+  return function (...)
+    return framework.awaitCB( func, ... )
+  end
 end
+
+
+framework.wait = framework.CreateSyncFunction(function(cb, time)
+                local timer = tmr.create()
+                print("timer:alarm new")
+                timer:alarm(time, tmr.ALARM_SINGLE, cb)
+              end)
+
 
 -- Start a new coroutine
+-- func   the function to run
+-- additional params are handed to func
 framework.start = function(func, ...)
   resumeFromCB(coroutine.create(func), unpack(arg))
 end
